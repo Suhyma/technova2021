04cf791199b62903d61b023e0ca511b8c259d737
Suhyma <56806431+Suhyma@users.noreply.github.com>
2021-08-29 08:59:22 -0400

    removed extra habits

--- js/popup.js
+++ js/popup.js
@@ -105,6 +105,7 @@ const progress2 = document.querySelector('.progress-done2');
 progress2.style.width = progress2.getAttribute('data-done') + '%';
 progress2.style.opacity = 1;
 
+/*
 const progress3 = document.querySelector('.progress-done3');
 progress3.style.width = progress3.getAttribute('data-done') + '%';
 progress3.style.opacity = 1;
@@ -112,7 +113,7 @@ progress3.style.opacity = 1;
 const progress4 = document.querySelector('.progress-done4');
 progress4.style.width = progress4.getAttribute('data-done') + '%';
 progress4.style.opacity = 1;
-
+*/
 
 /* TABS */
 
