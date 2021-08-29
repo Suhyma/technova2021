9a4a1a6b54367be431728dc43c81fac112702b57
Suhyma <56806431+Suhyma@users.noreply.github.com>
2021-08-29 00:09:18 -0400

    slight formatting changes

--- js/popup.js
+++ js/popup.js
@@ -48,12 +48,22 @@ function decrementValue(idName){
     value--;
     document.getElementById(idName).value = value;
 }
-const progress = document.querySelector('.progress-done');
 
-setTimeout(() => {
-  progress.style.opacity = 1;
-  progress.style.width = progress.getAttribute('data-done') + '%';
-}, 500)
+const progress = document.querySelector('.progress-done');
+progress.style.width = progress.getAttribute('data-done') + '%';
+progress.style.opacity = 1;
+
+const progress2 = document.querySelector('.progress-done2');
+progress2.style.width = progress2.getAttribute('data-done') + '%';
+progress2.style.opacity = 1;
+
+const progress3 = document.querySelector('.progress-done3');
+progress3.style.width = progress3.getAttribute('data-done') + '%';
+progress3.style.opacity = 1;
+
+const progress4 = document.querySelector('.progress-done4');
+progress4.style.width = progress4.getAttribute('data-done') + '%';
+progress4.style.opacity = 1;
 
 const Tabs = {
   init() {
